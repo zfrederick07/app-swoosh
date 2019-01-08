58556747ffa33f93236e95addfb00fb63c6e981d
Zachary Frederick <zfrederick6856@eagle.fgcu.edu>
2019-01-08 17:18:26 -0500

    Set up IBActions for buttons

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Zachary Frederick on 1/8/19.
+//  Copyright © 2019 Zachary Frederick. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
